a2a4b018ffe9ceee88521a19502495adca70dcad
Kvq5522 <89141917+Kvq5522@users.noreply.github.com>
2024-08-21 14:36:25 +0700

    init contract and client web

---
+++ client/src/main.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { Sepolia } from "@thirdweb-dev/chains";
+
+import { StateContextProvider } from "./context";
+import App from "./App";
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <ThirdwebProvider
+    activeChain={Sepolia}
+    supportedChains={[Sepolia]}
+    sdkOptions={{
+      gatewayUrls: [
+        "https://ipfs.io/ipfs/",
+        "https://gateway.ipfscdn.io/ipfs/",
+      ],
+    }}
+  >
+    <Router>
+      <StateContextProvider>
+        <App />
+      </StateContextProvider>
+    </Router>
+  </ThirdwebProvider>
+);
